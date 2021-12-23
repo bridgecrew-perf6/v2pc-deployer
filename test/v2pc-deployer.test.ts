9c4c52303c6a64e786c44b549fbdf43f26c32319
tovdata <dev@tovdata.com>
2021-12-23 09:29:44 +0900

    Initial commit

---
+++ test/v2pc-deployer.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as V2PcDeployer from '../lib/v2pc-deployer-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/v2pc-deployer-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new V2PcDeployer.V2PcDeployerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
