2972bad73c59f061fa8503d6064fea8375ade3e7
hmin01 <goals1003@gmail.com>
2022-01-03 11:05:15 +0900

    2022.01.03. - Remove account number and region information

--- bin/v2pc-deployer.ts
+++ bin/v2pc-deployer.ts
@@ -16,7 +16,7 @@ new V2PcDeployerStack(app, 'V2PcDeployerStack', {
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
-  env: { account: '395824177941', region: 'ap-northeast-2' }
+  env: { account: '', region: '' }
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
